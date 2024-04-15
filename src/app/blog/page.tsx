d82d94e0a9bf632e3ef8bfa3f79f526b7c3442f0
twentytwo44 <95289695+Twentytwo44@users.noreply.github.com>
2024-04-15 15:15:02 +0700

    commit

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,17 @@
+import exp from 'constants'
+import React from 'react'
+
+type Props = {}
+
+const Blog = (props: Props) => {
+  return (
+    <>
+
+    <div>
+        blog 
+    </div>
+    </>
+  )
+}
+
+export default Blog
---
+++ src/app/global_components/navbar.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Link from 'next/link'
+import { IoSearchOutline } from "react-icons/io5";
+
+type Props = {}
+
+const navbar = (props: Props) => {
+  return (
+   <>
+   <nav>
+    <div className='flex justify-between'>
+        <div>
+          logo
+        </div>
+        <div>
+           <form>
+              <div className='flex align-center w-full bg-[#EAECEE] p-3 rounded-lg'>
+                <input type="search" placeholder="ร้านอาหาร บทความ" className='outline-none b-0 bg-transparent '></input>
+                <span className=''><IoSearchOutline /></span>
+              </div>
+           </form>
+        </div>
+        <div>
+            <div>
+              favorites
+            </div>
+            <div>
+              profile
+            </div>
+        </div>
+    </div>
+   </nav>
+   
+   </>
+  );
+}
+
+export default navbar
