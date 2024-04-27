335d1fe915e9c46f109dd339f7a5273cdb18eb66
twentytwo44 <95289695+Twentytwo44@users.noreply.github.com>
2024-04-27 10:33:39 +0700

    cm

--- src/app/profile/information/page.tsx
+++ src/app/profile/information/page.tsx
@@ -1,11 +1,72 @@
-import exp from 'constants'
 import React from 'react'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>page</div>
+    <>
+     
+    <div className='flex flex-col gap-3' >
+      
+   
+             <div>
+                    <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                    <input 
+                    type="text" 
+                    id="username" 
+                    // onChange={(e) => setEmail(e.target.value)}
+                    
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#39DB4A] focus:border-[#39DB4A] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#39DB4A] dark:focus:border-[#39DB4A]" placeholder="email@email.com" required />
+            </div>
+            <div>
+                    <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                    <input 
+                    type="text" 
+                    id="email" 
+                    // onChange={(e) => setEmail(e.target.value)}
+                    
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#39DB4A] focus:border-[#39DB4A] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#39DB4A] dark:focus:border-[#39DB4A]" placeholder="email@email.com" required />
+            </div>
+            <div className='flex flex-row gap-3 w-full justify-between'>
+                <div  className='w-full'>
+                        <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Telephone</label>
+                        <input 
+                        type="text" 
+                        id="telephone" 
+                        // onChange={(e) => setEmail(e.target.value)}
+                        
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#39DB4A] focus:border-[#39DB4A] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#39DB4A] dark:focus:border-[#39DB4A]" placeholder="087-123457" required />
+                </div>
+                <div  className='w-full'>
+                    <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Birthday</label>
+                    <input 
+                    type="text" 
+                    id="birthday" 
+                    // onChange={(e) => setEmail(e.target.value)}
+                    
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#39DB4A] focus:border-[#39DB4A] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#39DB4A] dark:focus:border-[#39DB4A]" placeholder="email@email.com" required />
+                 </div>
+
+            </div>
+
+            <div>
+                    <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                    <input 
+                    type="password" 
+                    id="password" 
+                    // onChange={(e) => setPassword(e.target.value)}
+                    
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#39DB4A] focus:border-[#39DB4A] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#39DB4A] dark:focus:border-[#39DB4A]" placeholder="password" required />
+            </div>
+
+
+
+ 
+      
+    </div>
+   
+    </>
   )
 }
+
 export default page
