6f369f22d5f062fedefabf495d511c57c64a8a8c
twentytwo44 <95289695+Twentytwo44@users.noreply.github.com>
2024-04-28 01:11:05 +0700

    commit

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { signIn } from 'next-auth/react'
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 type Props = {}
 
@@ -11,15 +12,25 @@ const  LoginPage = (props: Props) => {
     
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const  Router = useRouter()
 
-    const handleSignIn = async () => {
-      
-        await signIn('credentials', {
-            email: email,
-            password: password,
-            redirect: true,
-            callbackUrl: '/'
-        })
+    const handleSignIn = async (e:any) => {
+        e.preventDefault()
+        try{
+            console.log(email)
+            console.log(password)
+            const result = await signIn('credentials', {
+                redirect: false,
+                email,
+                password
+            })
+            console.log(result)
+            Router.push('/profile')
+            
+        }
+        catch(err){
+            console.log(err)
+        }
     }
   return (
    <>
@@ -30,20 +41,22 @@ const  LoginPage = (props: Props) => {
             <div>
                     <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                     <input 
-                    type="text" 
+                    type="email" 
                     id="email" 
+                    value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#39DB4A] focus:border-[#39DB4A] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#39DB4A] dark:focus:border-[#39DB4A]" placeholder="email@email.com" required />
+                    required
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#39DB4A] focus:border-[#39DB4A] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#39DB4A] dark:focus:border-[#39DB4A]" placeholder="email@email.com"  />
             </div>
             <div>
                     <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                     <input 
                     type="password" 
                     id="password" 
+                    value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#39DB4A] focus:border-[#39DB4A] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#39DB4A] dark:focus:border-[#39DB4A]" placeholder="password" required />
+                    required
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#39DB4A] focus:border-[#39DB4A] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#39DB4A] dark:focus:border-[#39DB4A]" placeholder="password"  />
             </div>
             <button type="submit" className="w-full text-white bg-[#39DB4A] hover:bg-[#39DB4A]/90 focus:ring-3 focus:ring-[#39DB4A]/60 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-[#39DB4A]/60 dark:hover:bg-[#39DB4A]/70 focus:outline-none dark:focus:ring-[#39DB4A]/80">Sign in</button>
 
