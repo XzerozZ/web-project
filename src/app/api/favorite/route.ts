25b8befbde2e13ae654acc6c83acbdc1a38224d0
XzerozZ <111548296+XzerozZ@users.noreply.github.com>
2024-05-02 00:34:52 +0700

    back!!!

--- src/app/api/favorite/route.ts
+++ src/app/api/favorite/route.ts
@@ -34,10 +34,12 @@ export async function DELETE( req : Request ) {
     const prisma = new PrismaClient();
     try {
         const formData = await req.formData();
-        const id = parseInt(formData.get('id') as string);
         await prisma.favorite.delete({
             where : {
-                fav_id : id
+                user_id_res_id :{
+                    user_id : parseInt(formData.get('user_id') as string),
+                    res_id : parseInt(formData.get('res_id') as string)
+                }
             }
         })
         await prisma.$disconnect();
